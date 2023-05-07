c239b4bdbf00a38890b3528fcc369c4ee249f4bd
JingYJ0727 <jingtzn@gmail.com>
2023-05-07 17:21:06 +0800

    update

--- Admin_index.php
+++ Admin_index.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
+$servername = "proton-db-1.c3iemkdozdsc.us-east-1.rds.amazonaws.com";
 $username = "admin1234";
 $password = "admin1234";
-$dbname = "proton-db-1.c3iemkdozdsc.us-east-1.rds.amazonaws.com";
+$dbname = "Proton";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
