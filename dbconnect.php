67577a9d18906fd16af308a4d4b135355d221191
JingYJ0727 <jingtzn@gmail.com>
2023-05-07 17:03:20 +0800

    create

---
+++ dbconnect.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "54.234.159.234";
+$username = "admin1234";
+$password = "admin1234";
+$dbname = "proton-db-1.c3iemkdozdsc.us-east-1.rds.amazonaws.com";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}else{
+    echo "<script>console.log('Connection Success!')</script>";
+}
+?>
